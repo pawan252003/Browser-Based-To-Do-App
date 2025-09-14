152eea02e8654f19931a5f9efd1c955126610e10
pawan252003 <158072302+pawan252003@users.noreply.github.com>
2025-09-14 14:00:45 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db=SQLAlchemy()
 
 def create_app():
     app=Flask(__name__)
 
-    app.config["SECRET_KEY"]='My-secret-key'
-    app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///todo.db'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "my-secret-key")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI", "sqlite:///todo.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", "False")
     
     db.init_app(app)
 
@@ -18,6 +19,7 @@ def create_app():
     app.register_blueprint(tasks_bp)
 
     return app
+
 
 
 
