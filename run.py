bc93971359aea273574ec086c6c15d04b13daad0
pawan252003 <158072302+pawan252003@users.noreply.github.com>
2025-09-14 14:01:54 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from app.models import Task,User
+import os
 
 
 app=create_app()
@@ -8,4 +9,5 @@ with app.app_context():
     db.create_all()
 
 if __name__== "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default 5000
+    app.run(host="0.0.0.0", port=port)
